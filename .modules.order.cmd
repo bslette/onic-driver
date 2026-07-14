48927be91d4488adb6c282b13260547bb253770d
Branden Sletteland <bslettela@gmail.com>
2026-07-14 12:58:30 +0000

    chore: add gitignore for kernel module build artifacts and update device JSON definitions

--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo onic.o; :; } > modules.order
--- .onic.o.cmd
+++
@@ -1,3 +0,0 @@
-savedcmd_onic.o := ld -m elf_x86_64 -z noexecstack --no-warn-rwx-segments   -r -o onic.o @onic.mod  ; /usr/src/linux-headers-7.0.0-27-generic/tools/objtool/objtool --hacks=jump_label --hacks=noinstr --hacks=skylake --retpoline --rethunk --sls --stackval --static-call --uaccess --prefix=16  --link  --module onic.o
-
-onic.o: $(wildcard /usr/src/linux-headers-7.0.0-27-generic/tools/objtool/objtool)
